40656fe66fe6ba76fd8708baf1369972eb19c8b5
aliso <104593458+alisohub@users.noreply.github.com>
2026-07-28 15:45:56 +0300

    Update hub.js

--- hub.js
+++ hub.js
@@ -6,10 +6,8 @@
     }
     window.__scriptHubLoaded = true;
 
-    // const REPO_BASE_URL = 'https://raw.githubusercontent.com/alisohub/fc_amazon/refs/heads/main/scripts';
-    // development
-    const REPO_BASE_URL = 'https://raw.githubusercontent.com/alisohub/fc_amazon/refs/heads/development/scripts';
-
+    const REPO_BASE_URL = 'https://raw.githubusercontent.com/alisohub/fc_amazon/refs/heads/main/scripts';
+ 
     // Global Language State
     let currentLang = localStorage.getItem('sh_hub_lang') || 'EN';
 
